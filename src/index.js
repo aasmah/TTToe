bcb9122d416a29ab731a299094a8040a4adf41b6
aasmah <35153854+aasmah@users.noreply.github.com>
2023-05-15 17:13:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
-// ReactDOM.render(<Game />, document.getElementById('root'));
-window.addEventListener("DOMContentLoaded", function (e) {
-  // const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <Game />
-    </React.StrictMode>
-  );
-});
